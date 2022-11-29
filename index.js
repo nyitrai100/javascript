306e11a913b24262ca702d1d91e0e08a5be3d32d
nyitrai100 <nyitrai100@gmail.com>
2022-11-29 16:59:08 +0000

    .

--- index.js
+++ index.js
@@ -734,3 +734,26 @@
 
 
 // default option in switch statement
+
+function caseInSwitch(val) {
+    var answer ="";
+    switch(val){
+    case "a":
+        answer = "apple";
+        break;
+    case "b":
+        answer = "bird"
+        break;
+    case "c":
+        answer = "cat"
+        break;
+    default:
+        answer ="stuff";
+        break;
+
+    }
+
+    return answer;
+}
+
+console.log(caseInSwitch("a"));
