001bfe605415de90eb6ec22cc9bf54e7fe5da198
nyitrai100 <nyitrai100@gmail.com>
2022-11-29 10:38:12 +0000

    .

--- index.js
+++ index.js
@@ -676,32 +676,46 @@
 
 //golf code
 
-var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Gp Home!"]
-function golfScore(par, strokes) {
-    if (strokes == 1){
-        return names[0]
-    } else if (strokes <= par -2){
-        return names[1]
-    }
-    else if (strokes == par -1){
-        return names[2]
-    }
-    else if (strokes == par){
-        return names[3]
-    }
-    else if (strokes == par + 1){
-        return names[4]
-    }
-    else if (strokes == par + 2){
-        return names[5]
-    }
-    else if (strokes >= par + 3){
-        return names[6]
-    }
+// var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Gp Home!"]
+// function golfScore(par, strokes) {
+//     if (strokes == 1){
+//         return names[0]
+//     } else if (strokes <= par -2){
+//         return names[1]
+//     }
+//     else if (strokes == par -1){
+//         return names[2]
+//     }
+//     else if (strokes == par){
+//         return names[3]
+//     }
+//     else if (strokes == par + 1){
+//         return names[4]
+//     }
+//     else if (strokes == par + 2){
+//         return names[5]
+//     }
+//     else if (strokes >= par + 3){
+//         return names[6]
+//     }
 
 
 
-    return "Change me";
+//     return "Change me";
+// }
+
+// console.log(golfScore(5, -2));
+
+
+
+// Switch statement
+function caseInSwitch(val) {
+    var answer ="";
+    switch(val){
+        case 1
+    }
+
+    return answer;
 }
 
-console.log(golfScore(5, -2));
+caseInSwitch(1);
