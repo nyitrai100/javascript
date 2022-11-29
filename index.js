a1c56958b59891b5a249642ec6c3ed9191a38802
nyitrai100 <nyitrai100@gmail.com>
2022-11-29 18:13:17 +0000

    .

--- index.js
+++ index.js
@@ -787,25 +787,34 @@
 
 //replacing if else chanins with switch
 
-    function chainToSwitch(val){
-        var answer = "";
+    // function chainToSwitch(val){
+    //     var answer = "";
 
-    switch(val){
-        case "bob" :
-            answer = "Marley";
-            break;
-        case 42:
-            answer = "The answer";
-            break;
-        case 1:
-            answer = "There is no 1";
-            break;
-        case 99:
-            answer = "Missed me by this much!";
-            break;
-        case 7:
-            answer = "ate nine";
-            break;
-    }
-    }
-    console.log(chainToSwitch(1));
+    // switch(val){
+    //     case "bob" :
+    //         answer = "Marley";
+    //         break;
+    //     case 42:
+    //         answer = "The answer";
+    //         break;
+    //     case 1:
+    //         answer = "There is no 1";
+    //         break;
+    //     case 99:
+    //         answer = "Missed me by this much!";
+    //         break;
+    //     case 7:
+    //         answer = "ate nine";
+    //         break;
+    // }
+    // }
+    // console.log(chainToSwitch(1));
+
+// returning boolean values from functions
+
+function isLess(a, b) {
+    
+    return a < b;
+         
+}
+isLess(10, 15);
