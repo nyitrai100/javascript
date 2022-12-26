2f59cc224d3d8821671ba0564d2a7e245440d954
nyitrai100 <nyitrai100@gmail.com>
2022-12-26 17:58:43 +0000

    .

--- index.js
+++ index.js
@@ -855,17 +855,66 @@
 
 
 //Build a javascript Objects
-var ourDog = {
-    "name": "Camper",
+// var ourDog = {
+//     "name": "Camper",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["everything!"]
+// };
+
+// var myDog{
+//     "name": "Quincy",
+//     "legs": 3,
+//     "tails": 2,
+//     "friends": []
+
+// };
+
+//Accessing Object Properties with dot notation
+
+// var testObj = {
+//     "hat": "ballcap",
+//     "shirt": "jersey",
+//     "shoes": "cleats"
+// };
+
+// var hatValue = testObj.hat;
+// var shirtValue = testObj.shirt;
+
+
+
+//Accessing Object Properties with Bracket notation
+
+// var testObj = {
+//     "an entree": "hamburger",
+//     "my side": "veggies",
+//     "the drink": "water"
+// };
+
+// var entreeValue = testObj["an entree"];
+// var drinkValue = testObj["the drink"];
+
+
+
+
+//Accessing Object Properties with Variables
+
+// var testObj = {
+//     12: "Namath",
+//     16: "Montana",
+//     19: "Unitas"
+// };
+
+// var playerNumber = 16;
+// var player = testObj[playerNumber];
+
+
+//Uptading Object Properties
+
+var myDog = {
+    "name": "Coder",
     "legs": 4,
-    "tails": 1,
-    "friends": ["everything!"]
-};
+    "friends": ["freeCodeCamp Campers"]
+}
 
-var myDog{
-    "name": "Quincy",
-    "legs": 3,
-    "tails": 2,
-    "friends": []
-
-};
+myDog.name = "happy Coder";
