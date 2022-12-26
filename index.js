fb3fce0715f2e44aa3693516ff3db531db4ad6ca
nyitrai100 <nyitrai100@gmail.com>
2022-12-26 17:46:30 +0000

    .

--- index.js
+++ index.js
@@ -855,3 +855,17 @@
 
 
 //Build a javascript Objects
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+var myDog{
+    "name": "Quincy",
+    "legs": 3,
+    "tails": 2,
+    "friends": []
+
+};
