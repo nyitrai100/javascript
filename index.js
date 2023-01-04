b592af1a768a0259e3c204200178a42d2f522f40
nyitrai100 <nyitrai100@gmail.com>
2023-01-04 17:03:40 +0100

    .

--- index.js
+++ index.js
@@ -1372,13 +1372,36 @@
 
 //Mutate an Array Declared with const
 
-const s = [5, 7, 2];
-function editInPlace() {
-    "use strict";
+// const s = [5, 7, 2];
+// function editInPlace() {
+//     "use strict";
 
-    s[0] = 2;
-    s[1] = 5;
-    s[2] = 7;
+//     s[0] = 2;
+//     s[1] = 5;
+//     s[2] = 7;
+// }
+// editInPlace();
+// console.log(s);
+
+
+//Prevent Object Mutation
+
+function freezeObj() {
+    "use strict"
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+
+    Object.freeze(MATH_CONSTANTS);
+
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch( ex ) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
 }
-editInPlace();
-console.log(s);
+
+const PI = freezeObj();
+
+console.log(PI);
