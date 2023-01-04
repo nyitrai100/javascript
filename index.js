0e7a431729bca101767f44475892dda4b23c364c
nyitrai100 <nyitrai100@gmail.com>
2023-01-04 17:09:30 +0100

    .

--- index.js
+++ index.js
@@ -1409,4 +1409,14 @@
 
 //Use Arrow function to Write concise Anonymous Functions
 
-const magic = () => new Date();
+// const magic = () => new Date();
+
+
+
+//Write Arrow Functions with Parameters
+
+const myConcat = (arr1, arr2) => 
+    arr1.concat(arr2);
+
+
+console.log(myConcat([1, 2], [3, 4, 5]));
