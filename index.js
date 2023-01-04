d5188f0d65e28a0f977ea6154d0557795e99a03e
nyitrai100 <nyitrai100@gmail.com>
2023-01-04 14:52:11 +0100

    .

--- index.js
+++ index.js
@@ -1064,3 +1064,54 @@
 
 // var secondTree = myPlants[1].list[1];
 // console.log(secondTree);
+
+
+//record Collection
+
+var collection = {
+    "2548": {
+        "album": "slippery When Wet",
+        "artist": "Bon Jovi",
+        "Tracks": [
+            "Let it Rock",
+            "You give Love a Bad Name"
+    ]
+    },
+
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "Tracks": [
+            "1999",
+            "Little Red Corvette"
+    ]
+    },
+
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": [ ]
+    },
+
+    "5439": {
+        "album": "ABBA Gold"
+    }
+
+};
+
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value){
+    if (value === ""){
+        delete collection[id][prop];
+    } else if (prop === "tracks"){
+        collection[id][prop] = collection[id][prop] ||[];
+        collection[id][prop].push(value);
+    } else {
+        collection[id][prop] = value;
+    }
+
+    return collection;
+}
+
+updateRecords(2468, "tracks", "test"); 
+console.log(updateRecords(5439, "artist", "ABBA"));
