46164c28b8b1627bce72f9de3f13a4def082a4e9
nyitrai100 <nyitrai100@gmail.com>
2023-01-04 16:11:27 +0100

    .

--- index.js
+++ index.js
@@ -1238,41 +1238,52 @@
 
 //Profile Lookup
 
-var contacts = [
-    {
-        "firstName": "Akira",
-        "lastName": "Laine",
-        "number": "0543236543",
-        "likes": ["Pizza", "Coding", "Brownie Points"]
-    },
-    {
-        "firstName": "Harry",
-        "lastName": "Potter",
-        "number": "0994372684",
-        "likes": ["Hogwarts", "Magic", "Hagrid"]
-    },
-    {
-        "firstName": "Sherlock",
-        "lastName": "Holmes",
-        "number": "0487345643",
-        "likes": ["Intriguing Cases", "Violin"]
-    },
-    {
-        "firstName": "Kristian",
-        "lastName": "Vos",
-        "number": "unknown",
-        "likes": ["Javascript", "Gaming", "Foxes"]
-    },
-];
+// var contacts = [
+//     {
+//         "firstName": "Akira",
+//         "lastName": "Laine",
+//         "number": "0543236543",
+//         "likes": ["Pizza", "Coding", "Brownie Points"]
+//     },
+//     {
+//         "firstName": "Harry",
+//         "lastName": "Potter",
+//         "number": "0994372684",
+//         "likes": ["Hogwarts", "Magic", "Hagrid"]
+//     },
+//     {
+//         "firstName": "Sherlock",
+//         "lastName": "Holmes",
+//         "number": "0487345643",
+//         "likes": ["Intriguing Cases", "Violin"]
+//     },
+//     {
+//         "firstName": "Kristian",
+//         "lastName": "Vos",
+//         "number": "unknown",
+//         "likes": ["Javascript", "Gaming", "Foxes"]
+//     },
+// ];
 
-function lookUpProfile(name, prop) {
-    for(var i =0; i < contacts.length; i++){
-        if(contacts[i].firstName === name){
-            return contacts[i][prop] || "no such property";
-        }
-    }
-    return "no such contact";
+// function lookUpProfile(name, prop) {
+//     for(var i =0; i < contacts.length; i++){
+//         if(contacts[i].firstName === name){
+//             return contacts[i][prop] || "no such property";
+//         }
+//     }
+//     return "no such contact";
+// }
+
+// var data = lookUpProfile("Sherlock", "lastName");
+// console.log(data);
+
+
+
+//Generate Random Fractions
+
+function randomFraction() {
+
+    return Math.random();
 }
 
-var data = lookUpProfile("Sherlock", "lastName");
-console.log(data);
+console.log(randomFraction());
