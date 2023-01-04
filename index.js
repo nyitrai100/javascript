4231d7845f5147b2789b3a852a5d00983a7defcf
nyitrai100 <nyitrai100@gmail.com>
2023-01-04 17:46:30 +0100

    .

--- index.js
+++ index.js
@@ -1486,26 +1486,61 @@
 
 // Use Destructiuring Assignment to Assign Variables from Objects
 
-var voxel = { x: 3.6, y: 7.4, z: 6.54 };
+// var voxel = { x: 3.6, y: 7.4, z: 6.54 };
 
-var x = voxel.x;
-var y = voxel.y;
-var z = voxel.z;
+// var x = voxel.x;
+// var y = voxel.y;
+// var z = voxel.z;
 
-const { x : a, y : b, z : c } = voxel;
+// const { x : a, y : b, z : c } = voxel;
 
-const AVG_TEMPERATURES = {
-    today: 77.5,
-    tomorrow: 79
-};
+// const AVG_TEMPERATURES = {
+//     today: 77.5,
+//     tomorrow: 79
+// };
 
-function getTempOfTmrw(avgTemperatures){
-    "use strict"
+// function getTempOfTmrw(avgTemperatures){
+//     "use strict"
 
-    const { tomorrow : getTempOfTmrw } = avgTemperatures;
+//     const { tomorrow : getTempOfTmrw } = avgTemperatures;
     
-    return getTempOfTmrw;
-}
+//     return getTempOfTmrw;
+// }
 
-console.log(getTempOfTmrw(AVG_TEMPERATURES));
+// console.log(getTempOfTmrw(AVG_TEMPERATURES));
 
+
+
+
+//Destructuring Assignment with Nested Objects 
+
+// const LOCAL_FORECAST = {
+//     today: { min: 72, max: 83 },
+//     tomorrow: { min: 73.3, max : 84.6 }
+    
+// };
+
+// function getMaxOfTmrw(forecast) {
+//     "use strict";
+
+//     const {tomorrow : {max : maxOfTomorrow}} = forecast;
+
+//     return maxOfTomorrow;
+// }
+
+// console.log(getMaxOfTmrw(LOCAL_FORECAST));
+
+
+
+//Use Destructurin Assignment to assign Variables from Arrays
+
+const [z, x, , y] = [1, 2, 3, 4, 5, 6];
+console.log(z, x, y);
+
+let a = 8, b = 6;
+(() => {
+    "use strict";
+    [a, b] = [b, a]
+})();
+console.log(a);
+console.log(b);
