d95aab5d265a7ca6f07a962180f852235df11b11
nyitrai100 <nyitrai100@gmail.com>
2023-01-04 17:16:09 +0100

    .

--- index.js
+++ index.js
@@ -1415,8 +1415,22 @@
 
 //Write Arrow Functions with Parameters
 
-const myConcat = (arr1, arr2) => 
-    arr1.concat(arr2);
+// const myConcat = (arr1, arr2) => 
+//     arr1.concat(arr2);
 
 
-console.log(myConcat([1, 2], [3, 4, 5]));
+// console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+
+//Write Higher Order Arrow Functions
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+    return squaredIntegers;
+};
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
