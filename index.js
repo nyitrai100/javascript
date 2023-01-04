3ff89804b6ae7c0776b4dca80ced53e23bc85723
nyitrai100 <nyitrai100@gmail.com>
2023-01-04 14:15:23 +0100

    .

--- index.js
+++ index.js
@@ -1001,3 +1001,25 @@
 
 
 //Manipulating Complex Objects
+
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+        "CD",
+        "8T",
+        "LP"
+        ],
+        "gold":true
+    },
+    {
+        "artist": "Beau Carnes",
+        "title": "Cereal Man",
+        "release_year": 2003,
+        "formats": [
+            "Youtube video"
+        ]
+    }
+];
