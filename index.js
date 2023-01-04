bf53c9ed52cc1c58ec624f3c8cdacbeab15a9242
nyitrai100 <nyitrai100@gmail.com>
2023-01-04 17:19:59 +0100

    .

--- index.js
+++ index.js
@@ -1425,12 +1425,26 @@
 
 //Write Higher Order Arrow Functions
 
-const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+// const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 
-const squareList = (arr) => {
-    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
-    return squaredIntegers;
-};
+// const squareList = (arr) => {
+//     const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+//     return squaredIntegers;
+// };
 
-const squaredIntegers = squareList(realNumberArray);
-console.log(squaredIntegers);
+// const squaredIntegers = squareList(realNumberArray);
+// console.log(squaredIntegers);
+
+
+
+//Write Higher Order Arrow Functions
+
+const increment = (function(){
+    return function increment(number, value = 1){
+        return number + value;
+    };
+
+})();
+
+console.log(increment(5, 2));
+console.log(increment(5));
