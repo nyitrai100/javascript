e2e37041b136046969833046a674c7f6f64066c1
nyitrai100 <nyitrai100@gmail.com>
2023-01-04 16:22:41 +0100

    .

--- index.js
+++ index.js
@@ -1289,13 +1289,33 @@
 // console.log(randomFraction());
 
 
+
+
 //Generate Random Whole Numbers 
 
-var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+// var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
 
-function randomWholeNum(){
+// function randomWholeNum(){
 
-    return Math.floor(Math.random() * 10);
+//     return Math.floor(Math.random() * 10);
+// }
+
+// console.log(randomWholeNum());
+
+
+
+//Generate Random Whole Numbers within a Range
+
+function ourRandomRange(ourMin, ourMax) {
+
+    return Math.floor(Math.random()* (ourMax - ourMin + 1)) + ourMin;
+}
+ourRandomRange(1, 9);
+
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
 }
 
-console.log(randomWholeNum());
+var myRandom = randomRange(5, 15);
+
+console.log(myRandom);
