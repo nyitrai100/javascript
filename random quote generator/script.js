21d226e927d16f6627a90ee65e23b2cc29f4018f
nyitrai100 <nyitrai100@gmail.com>
2023-02-09 16:53:14 +0000

    .

--- random quote generator/script.js
+++ random quote generator/script.js
@@ -1,13 +1,16 @@
 function generate(){
     var quotes = {
         "- Albert Einstein" : '"Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and it"s beauty."',
-        "-Pierce Brown":'“Man cannot be freed by the same injustice that enslaved it.”',
-        "― C.S. Lewis":'“I was not born to be free---I was born to adore and obey.”',
+        "-Pierce Brown" : '“Man cannot be freed by the same injustice that enslaved it.”',
+        "― C.S. Lewis" : '“I was not born to be free---I was born to adore and obey.”',
     }
 
     var authors = Object.keys(quotes);
 
-    var author = authors[Math.floor(Math.random() * authors.length)]
+    var author = authors[Math.floor(Math.random() * authors.length)];
 
     var quote = quotes[author];
+
+    document.getElementById("quote").innerHTML = quote;
+    document.getElementById("author").innerHTML = author;
 }
