527293d0545d7eb6a7deeae7b361c60008a81f10
nyitrai100 <nyitrai100@gmail.com>
2023-02-09 16:36:42 +0000

    .

--- random quote generator/script.js
+++ random quote generator/script.js
@@ -1,3 +1,8 @@
 function generate(){
-    
+    var quotes = {
+        "- Albert Einstein" : '"Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and it"s beauty."',
+        "-Pierce Brown":'“Man cannot be freed by the same injustice that enslaved it.”',
+        "― C.S. Lewis";'“I was not born to be free---I was born to adore and obey.”',
+    }
 }
+
