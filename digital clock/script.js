4c77b0f13700416fd090b5a657ebdb7c5c28a2e0
nyitrai100 <nyitrai100@gmail.com>
2023-02-10 15:20:28 +0000

    .

---
+++ digital clock/script.js
@@ -0,0 +1,9 @@
+function calculateTime(){
+    var date = new Date();
+    var dayNumber = date.getDay();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var ampm = hour >= 12 ? 'Pm' : 'AM';
+    var dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+}
+
