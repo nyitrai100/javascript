56023570839b7648593b784e2798dee6107b223f
nyitrai100 <nyitrai100@gmail.com>
2023-02-16 16:44:51 +0000

    .

--- Grocery List /script.js
+++ Grocery List /script.js
@@ -4,4 +4,18 @@ const pencil = document.getElementById("pencil");
 
 const allItems = document.getElementById("allItems");
 
-const userInput = document.getElementById("userInput")
+const userInput = document.getElementById("userInput");
+
+pencil.addEventListener("click", function(){
+    allItems.innerHTML= "";
+})
+
+userInput.addEventListener("keydown", function(event){
+    if( event.key == "Enter")
+        addItem();
+})
+
+function addItem(){
+    var h2 = document.createElement("h2");
+    h2.innerHTML = "- " + userInput.value;
+}
