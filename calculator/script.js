13ba4c0bde21e50e3605f373da11909ce51b1839
nyitrai100 <nyitrai100@gmail.com>
2023-02-16 13:34:26 +0000

    js done, project done

--- calculator/script.js
+++ calculator/script.js
@@ -6,3 +6,13 @@ function press (num){
     expression += num;
     userInput.value = expression;
 }
+
+function equal(){
+    userInput.value = eval(expression);
+    expression = '';
+}
+
+function erase(){
+    expression = '';
+    userInput.value = expression;
+}
