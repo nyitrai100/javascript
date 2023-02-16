3678162f1187966270e818591d0036b4aa8760a9
nyitrai100 <nyitrai100@gmail.com>
2023-02-16 16:47:37 +0000

    .

--- Grocery List /script.js
+++ Grocery List /script.js
@@ -18,4 +18,10 @@ userInput.addEventListener("keydown", function(event){
 function addItem(){
     var h2 = document.createElement("h2");
     h2.innerHTML = "- " + userInput.value;
+
+    h2.addEventListener("click", function(){
+        h2.style.textDecoration = "line-through";
+    })
+
+    allItems.insertAdjacentElement("beforeend", h2);
 }
