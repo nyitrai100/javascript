abc594a2f037e2e614611355ce34f4d77e6b7176
nyitrai100 <nyitrai100@gmail.com>
2023-02-16 16:36:08 +0000

    .

--- Grocery List /script.js
+++ Grocery List /script.js
@@ -1,3 +1,7 @@
 const groceries = document.getElementsByClassName("groceries") [0];
 
 const pencil = document.getElementById("pencil");
+
+const allItems = document.getElementById("allItems");
+
+const userInput = document.getElementById("userInput")
