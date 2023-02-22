27a4d8e984d4bd1593e0ab016a8345ad1ead08e8
nyitrai100 <nyitrai100@gmail.com>
2023-02-22 14:45:21 +0000

    .

---
+++ tip calculeator/script.js
@@ -0,0 +1,19 @@
+const amount = document.getElementById('amount');
+const guests = document.getElementById('guests');
+const quality = document.getElementById('quality');
+const tipAmount = document.getElementById('tip-amount');
+
+
+// function calculate(){} - meaning the same like above
+
+calculate = () =>{
+    const tip = ((amount * quality.value) / (guests.value)).toFixed(2);
+    amount.value = '';
+    guests.value = '';
+    quality.value = '';
+    if( tip === 'NaN' ){
+        tipAmount.innerHTML = 'Tip 0$ each';
+        showTipAmount()
+    }
+    
+}
