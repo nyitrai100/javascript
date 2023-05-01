2a71e4022a1a63cfb069d6bed91f47ac120ed09d
nyitrai100 <nyitrai100@gmail.com>
2023-05-01 17:00:41 +0100

    done

--- search bar/script.js
+++ search bar/script.js
@@ -1,9 +1,23 @@
+
+
 const userCardTemplate= document.querySelector("[data-user-template]");
 const userCardContainer= document.querySelector("[data-user-cards-container]");
+const searchInput= document.querySelector("[data-search]");
+
+
+let users = [];
+
+searchInput.addEventListener("input", e =>{
+    const value = e.target.value.toLowerCase()
+    users.forEach(user =>{
+        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+        user.element.classList.toggle("hide", !isVisible)
+    })
+})
 
 fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(data => { 
     
-data.forEach(user => {
+users = data.map(user => {
 
 const card = userCardTemplate.content.cloneNode(true).children[0]
 const header = card.querySelector("[data-header]");
@@ -11,6 +25,7 @@ const body =  card.querySelector("[data-body]");
 header.textContent = user.name;
 body.textContent = user.email;
 userCardContainer.append(card)
+return { name: user.name, email: user.email, element: card}
 })
 })
 
